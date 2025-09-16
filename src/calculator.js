5138ae81a73c2226610454931fd9a5de28ee0c5e
CodewithAniket12 <aniketmadavi16@gmail.com>
2025-09-17 04:21:01 +0530

    handles two comma seperated numbers for add function

--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,11 @@
 export function add(numbers) {
-    if (numbers === "") {
+    if (numbers === '') {
         return 0;
     }
-    return parseInt(numbers);
+  
+    const numArray = numbers.split(',');
+    if (numArray.length === 1){
+        return parseInt(numbers);
+    }
+return parseInt(numArray[0])+parseInt(numArray[1]);
 }
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,9 +2,15 @@ import { test,expect} from "vitest";
 import {add} from "./calculator.js";
 
 test('should return 0 for empty string', () => {
-    expect(add("")).toBe(0);
+    expect(add('')).toBe(0);
 });
 
 test('should return the number itself for single number input', () =>{
-    expect(add("5")).toBe(5);
+    expect(add('5')).toBe(5);
 } );
+
+test(`should return the sum of two numbers`,()=>{
+    expect(add('12,11')).toBe(23);
+}
+
+);
