699b58fcf4ae48222e03b5d9e9688f0083de7a28
CodewithAniket12 <aniketmadavi16@gmail.com>
2025-09-17 03:56:44 +0530

    handles a single number for add function

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -4,3 +4,7 @@ import {add} from "./calculator.js";
 test('should return 0 for empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test('should return the number itself for single number input', () =>{
+    expect(add("5")).toBe(5);
+} );
