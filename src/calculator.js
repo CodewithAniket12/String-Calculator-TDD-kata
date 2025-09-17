fa6ac3b3f7786b41d2e6c7d5471f82ea3174b32a
CodewithAniket12 <aniketmadavi16@gmail.com>
2025-09-18 00:42:08 +0530

    handles different delimeters for add function

--- src/calculator.js
+++ src/calculator.js
@@ -1,10 +1,14 @@
 export function add(numbers) {
-    if (numbers === '') {
-        return 0;
-    }
+  if (numbers === '') {
+    return 0;
+  }
   
-  
-    const numArray = numbers.split(/[,\n]/);
-  return numArray.reduce((sum,num)=>sum+parseInt(num),0)
-    
+  let delimiter = /[,\n]/;
+  if (numbers.startsWith('//')) {
+    delimiter = numbers[2];
+    numbers = numbers.substring(numbers.indexOf('\n') + 1);
+  }
+
+  const numArray = numbers.split(delimiter);
+  return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -20,3 +20,7 @@ expect(add('1,2,3,4,5')).toBe(15);
 test('should handle new lines between numbers', () => {
   expect(add('1\n2,3')).toBe(6);
 });
+test('should handle different delimeters',()=>{
+    expect(add('//;\n2;3')).toBe(5);
+}
+);
